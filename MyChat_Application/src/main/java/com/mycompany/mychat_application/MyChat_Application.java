9cc15e35e08b0d56f6f45ac6f7c40cb2f02e4209
Ali Haider <141544052+ADHarry@users.noreply.github.com>
2024-03-01 19:48:21 +0500

    MyChat Application /  Java Netbeans

---
+++ MyChat_Application/src/main/java/com/mycompany/mychat_application/MyChat_Application.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mychat_application;
+
+/**
+ *
+ * @author Ali
+ */
+public class MyChat_Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
